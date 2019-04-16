fc85ec57a2f7580f2ba091493836c2111d1d5005
Richard <richard@protonmail.com>
2019-04-16 09:26:10 +0200

    Start org section and split components

--- containers/organization/OrganizationSection.js
+++ containers/organization/OrganizationSection.js
@@ -1,42 +1,27 @@
 import React from 'react';
 import { c } from 'ttag';
-import { Link } from 'react-router-dom';
-import {
-    SubTitle,
-    Alert,
-    Row,
-    Label,
-    Text,
-    SmallButton,
-    useModal,
-    useOrganization,
-    InputModal,
-    useApiWithoutResult
-} from 'react-components';
-import { updateOrganizationName } from 'proton-shared/lib/api/organization';
-import humanSize from 'proton-shared/lib/helpers/humanSize';
+import { SubTitle, Alert, Row, Label, Loader, useOrganization } from 'react-components';
+
+import OrganizationName from './OrganizationName';
+import ActivateOrganizationButton from './ActivateOrganizationButton';
 
 const OrganizationSection = () => {
-    const { request } = useApiWithoutResult(updateOrganizationName);
-    const [organization] = useOrganization();
-    const {
-        Name,
-        UsedMembers,
-        MaxMembers,
-        MaxSpace,
-        UsedVPN,
-        MaxVPN,
-        UsedDomains,
-        MaxDomains,
-        UsedAddresses,
-        MaxAddresses
-    } = organization;
-    const { isOpen, open, close } = useModal();
+    const [organization, loading] = useOrganization();
 
-    const handleSubmit = (name) => async () => {
-        await request(name);
-        close();
-    };
+    if (loading) {
+        return <Loader />;
+    }
+
+    if (!organization.HasKeys) {
+        return (
+            <>
+                <SubTitle>{c('Title').t`Multi-user support`}</SubTitle>
+                <Alert learnMore="https://protonmail.com/support/knowledge-base/business/">{c('Info')
+                    .t`Create and manage sub-accounts and assign them email addresses on your custom domain.`}</Alert>
+                <ActivateOrganizationButton />
+            </>
+        );
+    }
 
     return (
         <>
@@ -44,49 +29,7 @@ const OrganizationSection = () => {
             <Alert learnMore="todo">{c('Info').t`Lorem ipsum`}</Alert>
             <Row>
                 <Label>{c('Label').t`Organization name`}</Label>
-                <div>
-                    <Text className="mr1">{Name}</Text>
-                    <SmallButton onClick={open}>{c('Action').t`Edit`}</SmallButton>
-                    <InputModal
-                        show={isOpen}
-                        input={Name}
-                        title={c('Title').t`Change organization name`}
-                        label={c('Label').t`Organization name`}
-                        placeholder={c('Placeholder').t`Choose a name`}
-                        onClose={close}
-                        onSubmit={handleSubmit}
-                    />
-                </div>
-            </Row>
-            <Row>
-                <Label>{c('Label').t`Number of users`}</Label>
-                <Text>
-                    {`${UsedMembers}/${MaxMembers}`} <Link to="todo">{c('Link').t`Upgrade`}</Link>
-                </Text>
-            </Row>
-            <Row>
-                <Label>{c('Label').t`Organization storage`}</Label>
-                <Text>
-                    {humanSize(MaxSpace)} <Link to="todo">{c('Link').t`Upgrade`}</Link>
-                </Text>
-            </Row>
-            <Row>
-                <Label>{c('Label').t`VPN connections`}</Label>
-                <Text>
-                    {`${UsedVPN}/${MaxVPN}`} <Link to="todo">{c('Link').t`Upgrade`}</Link>
-                </Text>
-            </Row>
-            <Row>
-                <Label>{c('Label').t`Addresses`}</Label>
-                <Text>
-                    {`${UsedAddresses}/${MaxAddresses}`} <Link to="todo">{c('Link').t`Upgrade`}</Link>
-                </Text>
-            </Row>
-            <Row>
-                <Label>{c('Label').t`Domains`}</Label>
-                <Text>
-                    {`${UsedDomains}/${MaxDomains}`} <Link to="todo">{c('Link').t`Upgrade`}</Link>
-                </Text>
+                <OrganizationName organization={organization} />
             </Row>
         </>
     );
